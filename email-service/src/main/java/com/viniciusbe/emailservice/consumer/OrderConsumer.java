b298662b0949be8dcb5b9f5bac03ddf7d8be2f92
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-07-13 23:44:54 -0300

    feat (Email): create rabbitmq config, controller, consumer and dtos

---
+++ email-service/src/main/java/com/viniciusbe/emailservice/consumer/OrderConsumer.java
@@ -0,0 +1,17 @@
+package com.viniciusbe.emailservice.consumer;
+
+import com.viniciusbe.emailservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.queue.email.name}")
+    public void consume(OrderEvent event) {
+        LOGGER.info(String.format("Order event received -> %s", event.toString()));
+    }
+}
